cd12b0b2851d5dab49f7761f059e9cff953adc30
Jarrad <jarraj01@thryv.com>
2018-10-06 14:10:49 -0500

    uploading homework to github

---
+++ PyPoll/main.py
@@ -0,0 +1,54 @@
+import os
+from collections import Counter
+
+csvpath = os.path.join("..", "Resources", "election_data.csv")
+
+#Import csv
+import csv
+with open(csvpath, newline='') as csvfile:
+
+  # CSV reader delimiter and varaiable
+  csvreader = csv.reader(csvfile, delimiter=',')
+  next(csvreader,None)
+
+ #lists
+  voterid = []
+  county = []
+  candidates = []
+
+  #run for loop for every row
+  for dog in csvreader:
+      voterid.append(dog[0])
+      county.append(dog[1])
+      candidates.append(dog[2])
+
+  #find a unique set of canditates, and the total vote. Counter is a neat command
+  can_set = set(candidates)
+  tot_vote = len(voterid)
+  cnt = Counter(candidates)
+
+  #create a list with the unique names
+  can_names = []
+
+  for row in can_set:
+      can_names.append(row)
+
+  print("Election Results")
+  print("----------------------------------------")
+  print(f"The total number of votes was {tot_vote}")
+  print("----------------------------------------")
+
+  dictionary_can = {}
+  can_count = 0
+  for row in can_names:
+      candidate_name = str(can_names[can_count])
+      votes = candidates.count(candidate_name)
+      votes = int(votes)
+      percentage = round(votes / tot_vote * 100, 2)
+      dictionary_can.update({ candidate_name : votes})
+      print(f"{candidate_name}: {percentage}%  ({votes} votes)" )
+      can_count = can_count + 1
+
+  winner = max(dictionary_can, key=lambda key: dictionary_can[key])
+
+  print("Winner: ", winner)
